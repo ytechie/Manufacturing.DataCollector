861bda3e661fd61c3102b0f4d1d435715246fbad
Jason Young <jason@ytechie.com>
2014-09-05 10:13:08 -0500

    Moved the IDatasource to data collector

---
+++ IDatasource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Manufacturing.Framework.Datasource;
+
+namespace Manufacturing.DataCollector
+{
+    public interface IDatasource
+    {
+        event EventHandler<DataReceivedEventArgs<decimal>> DataReceived;
+
+        void StartRead();
+    }
+}
